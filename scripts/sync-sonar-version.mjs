9d6da8c656843fdcdb42f5c16cc6b0ef4cf9c0e9
MelkyWay <llsstorageone@gmail.com>
2026-03-27 22:27:14 +0100

    Add SonarQube config and package-version sync script

---
+++ scripts/sync-sonar-version.mjs
@@ -0,0 +1,13 @@
+﻿import { readFile, writeFile } from "node:fs/promises";
+
+const pkgPath = new URL("../package.json", import.meta.url);
+const sonarPath = new URL("../sonar-project.properties", import.meta.url);
+
+const pkgRaw = await readFile(pkgPath, "utf8");
+const pkg = JSON.parse(pkgRaw.replace(/^\uFEFF/, ""));
+
+let sonar = await readFile(sonarPath, "utf8");
+sonar = sonar.replace(/^sonar\.projectVersion=.*$/m, `sonar.projectVersion=${pkg.version}`);
+
+await writeFile(sonarPath, sonar, "utf8");
+console.log(`Updated sonar.projectVersion to ${pkg.version}`);
